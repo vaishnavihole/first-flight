bf5e062575387d6d40701084c2321957a2031eae
vaishnavihole <vaishnavihole1@gmail.com>
2023-05-18 18:11:56 +0530

    initial commit

---
+++ src/AswesomCard.js
@@ -0,0 +1,10 @@
+function AswesomCard() {
+  return (
+    <div>
+        <h1 style={{color: 'red',}}>Aswesom Card</h1>
+        <p>Aswesom card </p>
+    </div>
+  )
+}
+
+export default AswesomCard
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import AswesomCard  from './AswesomCard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<> <AswesomCard/></>);
+
